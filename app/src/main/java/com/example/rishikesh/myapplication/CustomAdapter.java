cb4ad951440b38b1e57ae0b099bdf470a6adff92
rishikksh20 <rishikksh20@gmail.com>
2016-02-01 04:46:39 +0530

    Fixing OutOfMemory problem while using bitMap by resizing the bitmap

--- app/src/main/java/com/example/rishikesh/myapplication/CustomAdapter.java
+++ app/src/main/java/com/example/rishikesh/myapplication/CustomAdapter.java
@@ -3,20 +3,25 @@ package com.example.rishikesh.myapplication;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
+import java.util.List;
+
 /**
  * Created by rishikesh on 1/2/16.
  */
-public class CustomAdapter extends ArrayAdapter<String> {
+public class CustomAdapter extends ArrayAdapter<Node> {
 
-    CustomAdapter(Context context, String[] images)
+    CustomAdapter(Context context, List<Node> nodeItems)
     {
-       super(context,R.layout.custom_row,images);
+       super(context,R.layout.custom_row,nodeItems);
 
     }
 
@@ -25,11 +30,36 @@ public class CustomAdapter extends ArrayAdapter<String> {
         LayoutInflater nodeInflater = LayoutInflater.from(getContext());
         View customView = nodeInflater.inflate(R.layout.custom_row, parent, false);
 
-        String nodeItems = getItem(position);
+        Node nodeItems = getItem(position);
         ImageView iv = (ImageView) customView.findViewById(R.id.imageView2);
-        Bitmap bp= BitmapFactory.decodeFile(nodeItems);
+        Bitmap bp= BitmapFactory.decodeFile(nodeItems.getImage());
+        //bp = scaleDown(bp,MAX_IMAGE_SIZE,true);
+        bp=scaleBitmap(bp,800,800);
         iv.setAdjustViewBounds(true);
         iv.setImageBitmap(bp);
         return customView;
+    }
+
+    public static Bitmap scaleDown(Bitmap realImage, float maxImageSize,
+                                   boolean filter) {
+        float ratio = Math.min(
+                (float) maxImageSize / realImage.getWidth(),
+                (float) maxImageSize / realImage.getHeight());
+        int width = Math.round((float) ratio * realImage.getWidth());
+        int height = Math.round((float) ratio * realImage.getHeight());
+
+        Bitmap newBitmap = Bitmap.createScaledBitmap(realImage, width,
+                height, filter);
+        return newBitmap;
+    }
+
+    public static Bitmap scaleBitmap(Bitmap bitmap, int wantedWidth, int wantedHeight) {
+        Bitmap output = Bitmap.createBitmap(wantedWidth, wantedHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        Matrix m = new Matrix();
+        m.setScale((float) wantedWidth / bitmap.getWidth(), (float) wantedHeight / bitmap.getHeight());
+        canvas.drawBitmap(bitmap, m, new Paint());
+
+        return output;
     }
 }
