d9d6d2dd0af3a355a9e0f643a6c8c4ec494f7eb3
rishikksh20 <rishikksh20@gmail.com>
2016-02-01 23:27:15 +0530

    Added mediaplayer functionality

--- app/src/main/java/com/example/rishikesh/myapplication/ImageShow.java
+++ app/src/main/java/com/example/rishikesh/myapplication/ImageShow.java
@@ -3,8 +3,10 @@ package com.example.rishikesh.myapplication;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -30,6 +32,35 @@ public class ImageShow extends AppCompatActivity {
         Bitmap bp= BitmapFactory.decodeFile(imageURL);
 
         showImage.setImageBitmap(bp);
+
+
+        //creating media player
+        final MediaPlayer mp=new MediaPlayer();
+        try{
+            //you can change the path, here path is external directory(e.g. sdcard) /Music/maine.mp3
+            mp.setDataSource(audioURL);
+
+            mp.prepare();
+        }catch(Exception e){e.printStackTrace();}
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.pause();
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.stop();
+            }
+        });
 
     }
 }
